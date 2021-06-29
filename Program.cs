73322e81256bcbaa26995a1a7457ecd639ce3de7
johannpa <johann_paque@hotmail.fr>
2021-06-29 14:04:03 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+
+            string url1 = "https://...";
+            string url2 = "https://...";
+
+            Console.Write("Téléchargement...");
+            var displayTask = DisplayProgress();
+            var downloadTask1 = DownloadData(url1);
+            var downloadTask2 = DownloadData(url2);
+
+
+            //await downloadTask1;
+            //await downloadTask2;
+
+            await Task.WhenAll(downloadTask1, downloadTask2);
+
+            Console.WriteLine("Fin du programme");
+
+
+            
+        }
+
+        static async Task DownloadData(string url)
+        {
+            var httpClient = new HttpClient();
+            
+
+            var resultat = await httpClient.GetStringAsync(url);
+            
+
+            Console.WriteLine($"OK -> {url}");
+
+            //Console.WriteLine(resultat);
+        }
+
+        static async Task DisplayProgress()
+        {
+            while (true)
+            {
+                await Task.Delay(500);
+                Console.Write(".");
+            }
+        }
+    }
+}
